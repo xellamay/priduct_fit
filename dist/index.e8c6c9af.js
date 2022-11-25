723c8b423490c058215e57992a728777b7880ec6
Alena <xellatroy@gmail.com>
2022-11-25 20:53:43 +0300

    правки в предыдущий коммит

--- dist/index.e8c6c9af.js
+++ dist/index.e8c6c9af.js
@@ -197,7 +197,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "62596" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "60128" + '/');
   ws.onmessage = function (event) {
     checkedAssets = {};
     assetsToAccept = [];
